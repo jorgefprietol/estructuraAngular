4d7c1297dfbcc162bf945e4bfe983d2f5b2a4ea8
Jorge Prieto <jorgefprietol@gmail.com>
2023-08-26 18:32:51 -0500

    mat-toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { InicioComponent } from './inicio.component';
 import { LibrosComponent } from './libros/libros/libros.component';
 import { LoginComponent } from './seguridad/login/login.component';
 import { RegistrarComponent } from './seguridad/registrar/registrar.component';
+import { UsuarioComponent } from './usuario.component';
 
 const routes: Routes = [
   { path:'', component: InicioComponent},
   { path: 'libros', component: LibrosComponent },
   { path: 'login', component: LoginComponent },
-  { path:'registrar', component: RegistrarComponent}
+  { path: 'registrar', component: RegistrarComponent },
+  { path:'usuario', component: UsuarioComponent}
 ];
 
 @NgModule({
