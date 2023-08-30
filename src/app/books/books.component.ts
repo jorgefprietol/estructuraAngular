e30ce3f340246723af3e6a1c6bdea589af40738e
Jorge Prieto <jorgefprietol@gmail.com>
2023-08-30 16:14:54 -0500

    ordenamiento tabla

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { BooksService } from './books.services';
 import { Books } from './books.model';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent implements OnInit {
+export class BooksComponent implements OnInit, AfterViewInit {
 
   booksData: Books[] = [];
   displayedColumns: string[] = ['titulo', 'descripcion', 'autor', 'precio'];
   dataSource = new MatTableDataSource<Books>();
+  @ViewChild(MatSort) ordenamiento: MatSort;
   constructor(private booksService: BooksService) { }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.ordenamiento;
+  }
 
   ngOnInit(): void {
     //this.booksData = this.booksService.obtenerLibros();
