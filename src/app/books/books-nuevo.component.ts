7aa75cb34431247a27fd8f5078e4dc5a19f7bff1
Jorge Prieto <jorgefprietol@gmail.com>
2023-09-01 17:27:26 -0500

    autores en books

--- src/app/books/books-nuevo.component.ts
+++ src/app/books/books-nuevo.component.ts
@@ -1,23 +1,29 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MatDatepicker } from '@angular/material/datepicker';
 import { BooksService } from './books.services';
 import { MatSelectChange } from '@angular/material/select';
 import { MatOption } from '@angular/material/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Autor } from '../autores/autor.model';
+import { AutoresService } from '../autores/autores.services';
 
 @Component({
   selector: 'app-book-nuevo',
   templateUrl: 'books-nuevo.component.html'
 })
 
-export class BooksNuevoComponent{
+export class BooksNuevoComponent implements OnInit{
   selectAutor: string;
   selectAutorTexto: string;
   fechaPublicacion: string;
   @ViewChild(MatDatepicker) picker: MatDatepicker<Date>;
+  autores: Autor[] = [];
 
-  constructor(private booksService: BooksService, private dialogRef: MatDialog){}
+  constructor(private booksService: BooksService, private dialogRef: MatDialog, private autoresService: AutoresService){}
+  ngOnInit(): void {
+    this.autores = this.autoresService.obtenerAutores();
+  }
 
   guardarLibro(form: NgForm) {
     if (form.valid) {
