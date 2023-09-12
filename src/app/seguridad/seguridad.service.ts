f8f8c3203df7b02e2437f985f1d2b29e38fe4339
Jorge Prieto <jorgefprietol@gmail.com>
2023-09-12 17:38:07 -0500

    token validation

--- src/app/seguridad/seguridad.service.ts
+++ src/app/seguridad/seguridad.service.ts
@@ -16,6 +16,32 @@ export class SeguridadService{
   private usuario: Usuario;
   segurdiadCambio = new Subject<boolean>();
 
+  cargarUsuario():void {
+    const tokenBrowser = localStorage.getItem('token');
+    if (!tokenBrowser) {
+      return;
+    }
+
+    this.token = tokenBrowser;
+    this.segurdiadCambio.next(true);
+    this.http.get<Usuario>(this.baseUrl + 'usuario')
+     .subscribe((response) => {
+       console.log('login respuesta', response);
+       this.token = response.token;
+       this.usuario = {
+         email: response.email,
+         nombre: response.nombre,
+         apellidos: response.apellidos,
+         token: response.token,
+         password: '',
+         username: response.username,
+         usuarioId: response.usuarioId
+       };
+       this.segurdiadCambio.next(true);
+       localStorage.setItem('token', response.token);
+    });
+  }
+
   obtenerToken(): string {
     return this.token;
   }
@@ -64,7 +90,8 @@ export class SeguridadService{
          username: response.username,
          usuarioId: response.usuarioId
        };
-      this.segurdiadCambio.next(true);
+       this.segurdiadCambio.next(true);
+       localStorage.setItem('token', response.token);
       this.router.navigate(['/']);
     });
   }
@@ -72,6 +99,7 @@ export class SeguridadService{
   salirSesion() {
     this.usuario = null;
     this.segurdiadCambio.next(false);
+    localStorage.removeItem('token');
     this.router.navigate(['/login']);
   }
 
@@ -80,6 +108,6 @@ export class SeguridadService{
   }
 
   onSesion() {
-    return this.usuario != null;
+    return this.token != null;
   }
 }
